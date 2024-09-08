6f24f92b3597e953615dd0d5d478cac91e2f7458
Luis Eduardo Rodríguez Ramírez <rodriguezramirezluiseduardo0@gmail.com>
2024-09-08 00:38:48 -0600

    Create simple authorization logic

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import pool from '../db'
 
 const router = express.Router()
 
@@ -7,5 +8,25 @@ router.get('/', (req, res) => {
         message: "Hello world"
     })
 })
+
+router.post<{},{},{user: string}>('/authenticate', async (req, res) => {
+    const {user} = req.body
+
+    const authenticate = await pool.query(
+        'SELECT * FROM USERS where nombre=$1;',
+        [user]
+    )
+    const response = authenticate.rowCount
+
+    if (response) {
+        res.json({
+            state: "authenticated succesfully"
+        })
+    } else {
+        res.status(404).json({
+            error: "Not found"
+        })
+    }
+} )
 
 export default router
