d61a8748a551e8c11709d20f520a6e123e4845bc
Luis Eduardo Rodríguez Ramírez <rodriguezramirezluiseduardo0@gmail.com>
2025-10-19 00:44:34 -0600

    update and restore config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,10 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
